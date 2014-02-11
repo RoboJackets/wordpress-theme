d5db88dc0916b8fe16a2a302d06196a514598b45
Justin Buchanan <justbuchanan@gmail.com>
2014-02-10 22:12:32 -0500

    changed facebook link address to fan page, not group

--- sidebar-clubinfo.php
+++ sidebar-clubinfo.php
@@ -3,7 +3,7 @@
 		<aside id="follow" class="widget">
 	          <h3>Follow Us</h3>
 		      <a href="<?php bloginfo('rss2_url'); ?>"><img src="<?php echo THEME_IMAGES; ?>/Feed_512x512.png" alt="RSS Feed" width="48" height="48" /></a>
-		      <a href="https://www.facebook.com/groups/27431371825/"><img src="<?php echo THEME_IMAGES; ?>/FaceBook_512x512.png" alt="Facebook" width="48" height="48" /></a>
+		      <a href="https://www.facebook.com/RoboJackets"><img src="<?php echo THEME_IMAGES; ?>/FaceBook_512x512.png" alt="Facebook" width="48" height="48" /></a>
 		      <a href="http://twitter.com/#!/RoboJackets"><img src="<?php echo THEME_IMAGES; ?>/Twitter_512x512.png" alt="Twitter" width="48" height="48" /></a>
 		      <a href="http://www.youtube.com/user/RoboJackets"><img src="<?php echo THEME_IMAGES; ?>/Youtube_512x512.png" alt="YouTube" width="48" height="48" /></a>
 	  	  </aside>
--- sidebar-media.php
+++ sidebar-media.php
@@ -3,7 +3,7 @@
 	      <p><strong>Check out our photo gallery!</strong></p>
 	      <p>See photos of our robots, events, and competitions.</p>
 	      <a href="<?php bloginfo('rss2_url'); ?>"><img src="<?php echo THEME_IMAGES; ?>/Feed_512x512.png" alt="RSS Feed" width="48" height="48" /></a>
-	      <a href="https://www.facebook.com/groups/27431371825/"><img src="<?php echo THEME_IMAGES; ?>/FaceBook_512x512.png" alt="Facebook" width="48" height="48" /></a>
+	      <a href="https://www.facebook.com/RoboJackets"><img src="<?php echo THEME_IMAGES; ?>/FaceBook_512x512.png" alt="Facebook" width="48" height="48" /></a>
 	      <a href="http://twitter.com/#!/RoboJackets"><img src="<?php echo THEME_IMAGES; ?>/Twitter_512x512.png" alt="Twitter" width="48" height="48" /></a>
 	      <a href="http://www.youtube.com/user/RoboJackets"><img src="<?php echo THEME_IMAGES; ?>/Youtube_512x512.png" alt="YouTube" width="48" height="48" /></a>
 
